7b01e42adab09466d4fb6809cc8875861d61427c
Eliyar Mamatjan <eliyar_m@hotmail.com>
2023-12-07 13:10:31 -0500

    Added friendly error message for duplicate identity error

--- src/components/ErrorDialog/enhanceMessage.ts
+++ src/components/ErrorDialog/enhanceMessage.ts
@@ -7,6 +7,8 @@ export default function enhanceMessage(message = '', code?: number) {
                 message +
                 '. Please make sure you are using the correct credentials.'
             )
+        case code === 53205: // Duplicate identity
+            return "Another attendee has joined the meeting using your name. If that wasn't you, please re-join the meeting with an alternate name."
         case message === 'Permission denied by system':
             return 'Unable to share your screen. Please make sure that your operating system has the correct permissions enabled for screen sharing.'
         default:
